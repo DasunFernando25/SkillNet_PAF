f723579b860326184931acc7f8684a8117568c60
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2025-04-15 20:58:18 +0530

    Add to main

---
+++ src/main/java/com/paf/skillnet_project/SkillnetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paf.skillnet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillnetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillnetProjectApplication.class, args);
+	}
+
+}
